834cf8f81a0757e7a9e9a881888d886c1124e168
oumayma <benhniyaoumayma@gmail>
2023-05-17 14:55:06 +0100

    oumayma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageLoginComponent} from "./pages/page-login/page-login.component";
 
-const routes: Routes = [];
+import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
+
+const routes: Routes = [
+
+  {
+   path: 'login',
+    component:PageLoginComponent
+  },
+  {
+    path: 'inscrire',
+    component:PageInscriptionComponent
+  },
+ /* {
+    path:'',
+    component:PageDashboardComponent,
+    children: [
+      {path: 'articles'}
+    ]
+  }*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
-import { PageInscriptionComponent } from './page-inscription/page-inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageLoginComponent,
-    PageInscriptionComponent
+    PageLoginComponent
   ],
   imports: [
     BrowserModule,
