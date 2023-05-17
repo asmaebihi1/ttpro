82a2720e50323f17e2e837a22052457e5ea3664b
oumayma <benhniyaoumayma@gmail>
2023-05-17 17:12:03 +0100

    oumayma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
+import {RouterModule, Routes} from '@angular/router';
+import { CommonModule  } from '@angular/common';
 
+const routes: Routes = [
+  { path: 'login', component: PageLoginComponent },
+  // Define other routes as needed
+];
 @NgModule({
   declarations: [
     AppComponent,
     PageLoginComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
--- src/app/pages/page-inscription/page-inscription.component.ts
+++ src/app/pages/page-inscription/page-inscription.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-page-inscription',
   templateUrl: './page-inscription.component.html',
